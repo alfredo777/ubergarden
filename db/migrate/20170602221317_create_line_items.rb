eec9890391ad50fcb9c7398028cafc0fed3221b4
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2017-06-08 00:13:01 -0500

    cambios en medida de campos de texto

--- db/migrate/20170602221317_create_line_items.rb
+++ db/migrate/20170602221317_create_line_items.rb
@@ -2,7 +2,7 @@ class CreateLineItems < ActiveRecord::Migration[5.0]
   def change
     create_table :line_items do |t|
       t.string :session_token
-      t.text :line_items, limit: 4294967296
+      t.text :line_items, limit: 1073741823
 
       t.timestamps
     end
--- db/migrate/20170602221423_create_productos_in_lines.rb
+++ db/migrate/20170602221423_create_productos_in_lines.rb
@@ -2,7 +2,7 @@ class CreateProductosInLines < ActiveRecord::Migration[5.0]
   def change
     create_table :productos_in_lines do |t|
       t.string :session_token
-      t.text :productos_in_line, limit: 4294967296
+      t.text :productos_in_line, limit: 1073741823
 
       t.timestamps
     end
