8e9005d157166d0a291eba6f8c7b441cc7cd0cf1
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2017-06-09 15:17:30 -0500

    validando parametros mobiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :mobile_device?
+
+  def mobile_device?
+    result  = request.env['HTTP_USER_AGENT']
+    puts result
+      if result =~ /iPhone|Android|iPad/
+        @browser = "Mobile"
+        @mobile = true
+      else
+      case 
+      when result =~ /Chrome/
+        @browser = "Google Chrome"
+        @mobile = false
+      when result =~ /Firefox/
+        @browser = "Firefox"
+        @mobile = false
+      when result =~ /Safari/
+        @browser = "Safari"
+        @mobile = false
+      when result =~ /MSIE/
+        @browser = "Internet Explorer"
+        @mobile = false        
+      end 
+      end
+    puts "********************** #{@browser} / Mobile: #{@mobile} ************************" 
+       @mobile
+  end
 end
