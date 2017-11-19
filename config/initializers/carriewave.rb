e1f08c535292951558cf47a66852c42d39608297
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2017-11-19 14:18:14 -0600

    cambios en carriewave

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -31,7 +31,7 @@ CarrierWave.configure do |config|
     config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
     config.fog_directory  = 'urgarden'
     config.fog_public     = false 
-    config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+    #config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
     #config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
   end
 end
