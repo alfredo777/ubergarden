36ef181bf5ab71d2720c8c992dcae96ff79e2f58
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2017-11-30 16:32:32 -0600

    cambios en llaves publicas de produccinnn conneckta

--- config/initializers/coneckta.rb
+++ config/initializers/coneckta.rb
@@ -1,5 +1,11 @@
+
+if Rails.env == 'development'
 $PUBLIC_KEY_CONEKTA = "key_M3cVm2vHFHcGheiLsQAG1sg"
 $PRIVATE_KEY_CONEKTA = "key_VBHU53fRueVKhyG8WrNHYg"
+else
+$PUBLIC_KEY_CONEKTA = "key_exdkyZDp1qKhk4xkugHY1Aw"
+$PRIVATE_KEY_CONEKTA = "key_MVgA2Xxiuu3mzHWPrJjnbA"
+end
 
 
 Conekta.config do |c|
